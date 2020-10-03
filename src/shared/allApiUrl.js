d59db6a6576c8a3a0c8c7d5bfb98e0adb76128e6
amitnitjsr <amitnips007@gmail.com>
2020-10-03 18:37:12 +0530

    Added component

---
+++ src/shared/allApiUrl.js
@@ -0,0 +1,11 @@
+export const LOGIN_URL = '';
+export const LOGOUT_URL = '';
+export const PROFILE_URL = '';
+export const USER_URL = '';
+export const UNIVERSITY_URL = '';
+export const CHANGE_PASSWORD_URL = '';
+export const SETTINGS_URL = '';
+export const CMS_URL = '';
+export const EMAIL_URL = '';
+export const ORDER_URL = '';
+export const TRIP_URL = '';
---
+++ src/shared/helpers.js
@@ -0,0 +1,20 @@
+export const apiBaseUrl = `http://127.0.0.1:8000/`;
+export const updateObject = (oldObj, newObj) => {
+    console.log({ ...oldObj, ...newObj })
+    return { ...oldObj, ...newObj };
+}
+
+export const getMethod = (type)=>{
+    let obj = {
+        ADD:"post",
+        UPDATE:"put",
+        DELETE:"delete",
+        GET:"get",
+        GET_ALL:"get"
+    }
+    return obj[type];
+}
+
+export const getAuthToken = localStorage.getItem("access-token");
+export const getAuthUserId = localStorage.getItem("login-userId");
+export const getImageUrl = imgUrl => apiBaseUrl+imgUrl;
